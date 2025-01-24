076ffc99d1612607d66cdf445c9517e7c295ac27
Karl K <da1men@freenet.de>
2025-01-24 18:06:05 +0100

    Junit 5 hinzugefügt

---
+++ timetracker/src/test/java/de/karlk/timetracker/AClassWithOneJUnitTest.java
@@ -0,0 +1,12 @@
+package de.karlk.timetracker;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AClassWithOneJUnitTest {
+	@Test
+    void demoTestMethod() {
+        assertTrue(true, "Wahrheitswert True erwartet");
+    }
+}

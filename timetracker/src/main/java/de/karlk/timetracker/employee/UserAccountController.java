e9be6808306cebbc34efde11584f5353fc4d664a
Karl K <da1men@freenet.de>
2025-03-26 16:41:11 +0100

    HAL Linking für UserAccounts implementiert

--- timetracker/src/main/java/de/karlk/timetracker/employee/UserAccountController.java
+++ timetracker/src/main/java/de/karlk/timetracker/employee/UserAccountController.java
@@ -1,25 +1,44 @@
 package de.karlk.timetracker.employee;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class UserAccountController {
 	@Autowired UserAccountRepository userAccountRepository;
 	
-	@GetMapping("/users")
-	List<UserAccount> all(){
+	@GetMapping("/userAccounts")
+	List<UserAccount> allNotRestful(){
 		return userAccountRepository.findAll();
 	}
 	
+	@GetMapping("/users")
+	CollectionModel<EntityModel<UserAccount>> all(){
+		List<EntityModel<UserAccount>> users = userAccountRepository.findAll().stream().map(userAccount -> EntityModel.of(userAccount,
+				linkTo(methodOn(UserAccountController.class).one(userAccount.getName())).withSelfRel(),
+				linkTo(methodOn(UserAccountController.class).all()).withRel("users")))
+				.collect(Collectors.toList());
+		
+		return CollectionModel.of(users, linkTo(methodOn(UserAccountController.class).all()).withSelfRel());
+	}
 	
 	@GetMapping("/users/{userAccountName}")
-	UserAccount one(@PathVariable String userAccountName){
-		return userAccountRepository.findFirstByName(userAccountName)
+	EntityModel<UserAccount> one(@PathVariable String userAccountName){
+		
+		UserAccount acc = userAccountRepository.findFirstByName(userAccountName)
 				.orElseThrow(() -> new UserAccountNotFoundException(userAccountName));
+		
+		return EntityModel.of(acc, 
+				linkTo(methodOn(UserAccountController.class).one(userAccountName)).withSelfRel(),
+				linkTo(methodOn(UserAccountController.class).all()).withRel("users"));
 	}
 }
